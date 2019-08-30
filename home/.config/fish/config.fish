de8bdbca3e0143b407ecdf5679b9431738335fc3
Ben <ben@alwaysblank.org>
2019-08-30 15:32:42 -0700

    Slight updates to config.fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,9 +1,8 @@
-. "$HOME/.homesick/repos/homeshick/homeshick.fish"
-set -g -x VISUAL 'vim'
-set -g -x EDITOR 'vim'
 if status --is-interactive
   set PATH $HOME/.rbenv/bin $PATH
   . (rbenv init - | psub)
 end
 set PATH $HOME/.cargo/bin $PATH
+set -g -x EDITOR 'vim'
+set -g -x VISUAL 'vim'
 eval (starship init fish)
