3a40dc5122925449ee680cce9e017334dcc026c0
Ben Martinez-Bateman <ben@alwaysblank.org>
2019-11-16 14:42:07 -0800

    some new local updates from illyria

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,8 +1,10 @@
 if status --is-interactive
-  set PATH $HOME/.rbenv/bin $PATH
+  set --universal fish_user_paths $fish_user_paths ~/.rbenv/bin
   . (rbenv init - | psub)
 end
-set PATH $HOME/.cargo/bin $PATH
+set --universal fish_user_paths $fish_user_paths ~/.cargo/bin
+set --universal fish_user_paths $fish_user_paths ~/.scripts
+set --universal fish_user_paths $fish_user_paths ~/.config/composer/vendor/bin
 set -g -x EDITOR 'vim'
 set -g -x VISUAL 'vim'
 eval (starship init fish)
