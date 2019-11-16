38598f968b9973bb8ed3d13aa90177a9625140af
Ben Martinez-Bateman <ben@alwaysblank.org>
2019-11-16 14:46:49 -0800

    ammend path in a responsible way

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,10 +1,10 @@
 if status --is-interactive
-  set --universal fish_user_paths $fish_user_paths ~/.rbenv/bin
+  set --global fish_user_paths $fish_user_paths ~/.rbenv/bin
   . (rbenv init - | psub)
 end
-set --universal fish_user_paths $fish_user_paths ~/.cargo/bin
-set --universal fish_user_paths $fish_user_paths ~/.scripts
-set --universal fish_user_paths $fish_user_paths ~/.config/composer/vendor/bin
+set --global fish_user_paths $fish_user_paths ~/.cargo/bin
+set --global fish_user_paths $fish_user_paths ~/.scripts
+set --global fish_user_paths $fish_user_paths ~/.config/composer/vendor/bin
 set -g -x EDITOR 'vim'
 set -g -x VISUAL 'vim'
 eval (starship init fish)
