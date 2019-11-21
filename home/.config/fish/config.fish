83aaa13b2957fe512803ee67b5bb488f0e801693
Ben Martinez-Bateman <ben@alwaysblank.org>
2019-11-21 09:47:02 -0800

    Just add to path, don't bother with fish paths

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,9 +2,7 @@ if status --is-interactive
   set --global fish_user_paths $fish_user_paths ~/.rbenv/bin
   . (rbenv init - | psub)
 end
-set --global fish_user_paths $fish_user_paths ~/.cargo/bin
-set --global fish_user_paths $fish_user_paths ~/.scripts
-set --global fish_user_paths $fish_user_paths ~/.config/composer/vendor/bin
+set PATH ~/.cargo/bin ~/.scripts ~/.config/composer/vendor/bin $PATH
 set -g -x EDITOR 'vim'
 set -g -x VISUAL 'vim'
 eval (starship init fish)
