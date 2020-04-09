b6ea1b529fd1a4ee5a6ce2e1b4e6afc38d19e38e
Ben Martinez-Bateman <ben@alwaysblank.org>
2020-04-09 10:43:43 -0700

    Fix loading rvm and composer a bit

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,8 +1,8 @@
 if status --is-interactive
   set PATH ~/.rbenv/bin $PATH
-  . (rbenv init - | psub)
+  source (rbenv init - | psub)
 end
-set PATH ~/.cargo/bin ~/.scripts ~/.config/composer/vendor/bin $PATH
+set PATH ~/.cargo/bin ~/.scripts ~/.config/composer/vendor/bin ~/.composer/vendor/bin $PATH
 set -g -x EDITOR 'vim'
 set -g -x VISUAL 'vim'
 eval (starship init fish)
